3456cb4c95b718e1bbeda4ea2f177250a6df84a6
Turqay Umudzade <turgayau@code.edu.az>
2020-04-28 07:06:12 +0400

    debugging neighbor counter

--- game.c
+++ game.c
@@ -4,43 +4,40 @@
 
 int isValidCell(int x, int y, int r, int c)
 {
-    if ((x - 1 >= 0 && x + 1 < r) && (y - 1 > 0 && y + 1 < c))
-
+    if ((x >= 0 && x < r) && (y >= 0 && y < c))
         return 1;
     else
-        return 0
+        return 0;
 }
 
 int aliveNeighborCount(int i, int j, int a[col][row])
 {
     int counter = 0;
-    //TOP
-    if (a[i - 1][j - 1] == 1 )
-        counter++;
-    if (a[i - 1][j] == 1)
-        counter++;
-    if (a[i - 1][j + 1] == 1)
-        counter++;
-    //MIDDLE
-    if (a[i][j - 1] == 1)
-        counter++;
-    if (a[i][j + 1] == 1)
-        counter++;
-    //BOTTOM
-    if (a[i + 1][j - 1] == 1)
-        counter++;
-    if (a[i + 1][j] == 1)
-        counter++;
-    if (a[i + 1][j + 1] == 1)
-        counter++;
+    int x = -1, y = -1;
+    int neighbors[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, //TOP
+                        {0, -1}, {0, 1},//LEFT N RIGHT
+                        {1, -1}, {1, 0}, {1, 1}};//BOTTOM
+
+    for (int k = 0; k < 8; k++)
+    {
+        x = i + neighbors[k][0];
+        y = j + neighbors[k][1];
+        if (isValidCell(x, y, col, row))//check if doesnt cross the borders
+        {
+            if (a[x][y] == 1)
+                counter++;
+        }
+    }
 
     return counter;
 }
 
 int main(int argc, char const *argv[])
 {
+    //TESTING
     int arena[8][8] = {0};
     arena[1][0] = 1;
+    arena[1][2] = 1;
     printf("%d", aliveNeighborCount(1, 1, arena));
     return 0;
 }
