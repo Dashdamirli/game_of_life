a2df44f3f3f1b0235dfeb82f58a5cebf8c241723
Turqay Umudzade <turgayau@code.edu.az>
2020-04-28 06:09:24 +0400

    table with default size created

---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define ROW 8
+#define COLUMN 8
+int main(){
+   int arena [ROW][COLUMN] = {0};
+   int i, j;
+   
+   
+   for(i=0; i<ROW; i++) {
+      for(j=0;j<COLUMN;j++) {
+         printf("%d ", arena[i][j]);
+         if(j==COLUMN-1){
+             printf("\n");
+         }
+      }
+   }
+   return 0;
+}
