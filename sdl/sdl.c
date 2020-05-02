93c667ee8e0d713e0ab2cd549ae272dae61e2f86
Dashdamirli <dashdemirli@gmail.com>
2020-05-02 04:03:01 -0400

    some improvements on both cmake and sdl

--- sdl/sdl.c
+++ sdl/sdl.c
@@ -1,80 +1,6 @@
-// #include "sdl.h"
-#include <SDL2/SDL.h> 
-#include <SDL2/SDL_image.h>
+#include "sdl.h"
 
-int drawPixel(){
-    int quit = 1;
-
-    int leftMouseButtonDown = 0;
-
-    SDL_Event event;
-
-    SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window * window = SDL_CreateWindow("SDL2 Pixel Drawing",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
-    
-    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
-
-    SDL_Texture * texture = SDL_CreateTexture(renderer,
-        SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 640, 480);
-
-    int pixels[307200];
-
-    memset(pixels, 255, 640 * 480 * sizeof(Uint32));
-
-    while (quit){
-
-        SDL_UpdateTexture(texture, NULL, pixels, 640 * sizeof(Uint32));
-
-
-        SDL_WaitEvent(&event);
-
-        switch (event.type){
-            case SDL_QUIT:
-                quit = 0;
-            break;
-            
-            case SDL_MOUSEBUTTONUP:
-                if (event.button.button == SDL_BUTTON_LEFT)
-                    leftMouseButtonDown = 0;
-                break;
-
-            case SDL_MOUSEBUTTONDOWN:
-                if (event.button.button == SDL_BUTTON_LEFT)
-                    leftMouseButtonDown = 1;
-            case SDL_MOUSEMOTION:
-                if (leftMouseButtonDown){
-                    int mouseX = event.motion.x;
-                    int mouseY = event.motion.y;
-                    pixels[mouseY * 640 + mouseX] = 0;
-                }
-                break;
-        }
-        
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
-
-    }
-
-
-    SDL_DestroyTexture(texture);
-
-    SDL_DestroyRenderer(renderer);
-
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
-
-    return 0;
-}
-
-
-// int main(int argc, char ** argv)
-
-// {
-
+// int drawPixel(){
 //     int quit = 1;
 
 //     int leftMouseButtonDown = 0;
@@ -140,5 +66,77 @@ int drawPixel(){
 //     SDL_Quit();
 
 //     return 0;
-
 // }
+
+
+int main(int argc, char ** argv)
+
+{
+
+    int quit = 1;
+
+    int leftMouseButtonDown = 0;
+
+    SDL_Event event;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window * window = SDL_CreateWindow("SDL2 Pixel Drawing",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_Texture * texture = SDL_CreateTexture(renderer,
+        SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 640, 480);
+
+    int pixels[307200];
+
+    memset(pixels, 255, 640 * 480 * sizeof(Uint32));
+
+    while (quit){
+
+        SDL_UpdateTexture(texture, NULL, pixels, 640 * sizeof(Uint32));
+
+
+        SDL_WaitEvent(&event);
+
+        switch (event.type){
+            case SDL_QUIT:
+                quit = 0;
+            break;
+            
+            case SDL_MOUSEBUTTONUP:
+                if (event.button.button == SDL_BUTTON_LEFT)
+                    leftMouseButtonDown = 0;
+                break;
+
+            case SDL_MOUSEBUTTONDOWN:
+                if (event.button.button == SDL_BUTTON_LEFT)
+                    leftMouseButtonDown = 1;
+            case SDL_MOUSEMOTION:
+                if (leftMouseButtonDown){
+                    int mouseX = event.motion.x;
+                    int mouseY = event.motion.y;
+                    pixels[mouseY * 640 + mouseX] = 0;
+                }
+                break;
+        }
+        
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+
+    }
+
+
+    SDL_DestroyTexture(texture);
+
+    SDL_DestroyRenderer(renderer);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
+
+}
